2103326a9f9c85d37f6ceae7e2188a967534320b
Elsa San <esan5574@uni.sydney.edu.au>
2021-05-16 02:14:56 +1000

    added stopwatch js

--- public/components/stopwatch.js
+++ public/components/stopwatch.js
@@ -12,3 +12,42 @@ for (i = 0; i < acc.length; i++) {
           }
         });
 }
+
+//referred to https://dev.to/stackfindover/how-to-create-a-stopwatch-in-javascript-57a8
+
+const watch = document.querySelector("#a-stopwatch")
+let millisecound = 0;
+let timer;
+
+function timeStart(){
+  clearInterval(timer);
+  timer = setInterval(() => {
+    millisecound += 10;
+
+    let dateTimer = new Date(millisecound);
+
+    watch.innerHTML = 
+    ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
+    ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
+    ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
+  }, 10);
+}
+
+
+function timePaused() {
+  clearInterval(timer);
+}
+
+function timeReset(){
+  setInterval(timer)
+  millisecound = 0;
+  watch.innerHTML = "00:00:00";
+}
+
+document.addEventListener('click', (e) => {
+  const el = e.target;
+
+  if(el.id === 'start') timeStart();
+  if(el.id === 'pause') timePaused();
+  if(el.id === 'reset') timeReset();
+})
